c702935a120bc9e10c7ebf2d4b124d1ffc013713
ArpitKansal01 <kansalarpit06@gmail.com>
2026-05-07 16:25:09 +0530

    added a lot of changes

--- controllers/summarizeTranslate.js
+++ controllers/summarizeTranslate.js
@@ -1,7 +1,7 @@
 const { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
+  apiKey: process.env.GEMINI_API_KEY2,
 });
 
 exports.summarizeTranslate = async (req, res) => {
